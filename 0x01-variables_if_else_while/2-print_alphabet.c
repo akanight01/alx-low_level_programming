95e481038edd33eaefa4ff5dc49fa062b699af23
akanight01 <134161622+akanight01@users.noreply.github.com>
2023-07-15 13:45:13 +0100

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success/correct)
- */
+*main - Program to print alphabet letter followed by new line
+*
+*Return: return 0
+*/
 int main(void)
-
 {
-	char alphabet;
-
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-	putchar(alphabet);
-	}
-
-	putchar('\n');
-
-	return (0);
+int letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+{
+letter = tolower(letter);
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
