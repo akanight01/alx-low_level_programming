817a6f36c0b76ff035d45a11aff59ae4aaf50089
akanight01 <akanight2000@yahoo.com>
2023-08-05 09:28:31 -0700

    statics library

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * _strchr - locates a character in a string,
- * @s: string.
- * @c: character.
- * Return: the pointer to the first occurrence of the character c.
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: void
  */
-char *_strchr(char *s, char c)
-{
-	unsigned int i = 0;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
 }
