6e12dca8cbc9b363fb64488d3e86b967a69418da
akanight01 <134161622+akanight01@users.noreply.github.com>
2023-08-11 07:22:30 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,115 +1,156 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-int find_len(char *str);
-char *create_xarray(int size);
-char *iterate_zeroes(char *str);
-void get_prod(char *prod, char *mult, int digit, int zeroes);
-void add_nums(char *final_prod, char *next_prod, int next_len);
-
 /**
-* find_len - finds the length of a string.
-* @str: The string to be measured.
-*
-* Return: The length of the string.
-*/
-int find_len(char *str)
+ * str_len - finds string length
+ * @str: input pointer to string
+ * Return: length of string
+ */
+int str_len(char *str)
 {
-	int len = 0;
-	
-	while (*str++)
-		len++;
-	
-	return(len);
+	int len;
+
+	for (len = 0; *str != '\0'; len++)
+		len++, str++;
+	return (len / 2);
 }
-
 /**
-* Create_xarry - Creates an array of chars and initializes it with the character 'x'. Adds a terminating null byte.
-* @size: The size of the array to be initialized.
-*
-*Description: If there is insufficient space, the function exits with a status of 98.
-*Return: A pointer to the array.
-*/
-char *Create_Xarry(int size)
+ * _calloc - allocates memory for an array using malloc
+ * @bytes: bytes of memory needed per size requested
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
+ */
+void *_calloc(unsigned int bytes, unsigned int size)
 {
-	char *array;
-	int index;
-	
-	array = malloc(sizeof(char) * size);
-	
-	if(array == NULL)
-		exit(98);
-	
-	for(index = 0; index < (size - 1); index++)
-		array[index] = 'x';
-	array[index] = '\0';
-	
-	return(array);
+	unsigned int i;
+	char *p;
+
+	if (bytes == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * bytes);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < bytes * size; i++)
+		p[i] = 0;
+	return ((void *)p);
 }
-
 /**
-* iterate_zeroes - Iterates through a string of numbers containing leading zeroes untill it a non-zero number.
-* @str: The string of numbers to be iterate through.
-*
-* Return: A pointer to the next non-zero element.
-*/
-char *iterate_zeroes(char *str)
+ * add_arrays - adds 2 arrays of ints
+ * @mul_result: pointer to array with numbers from product
+ * @sum_result: pointer to array with numbers from total sum
+ * @len_r: length of both arrays
+ * Return: void
+ */
+void add_arrays(int *mul_result, int *sum_result, int len_r)
 {
-	while (*str && *str == '0')
-		str++;
-	
-	Return (str);
+	int i = 0, len_r2 = len_r - 1, carry = 0, sum;
+
+	while (i < len_r)
+	{
+		sum = carry + mul_result[len_r2] + sum_result[len_r2];
+		sum_result[len_r2] = sum % 10;
+		carry = sum / 10;
+		i++;
+		len_r2--;
+	}
 }
-
 /**
-* get_digit - Converts a digit character to a corresponding int.
-* @c: The character to be converted.
-*
-* Description: If c is a non-digit, the function exits with a status of 98.
-* Return: The Converted int.
-int get_digits(char c)
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
 {
-	int digit = c - '0';
-	
-	If(digit < 0 || digit > 9)
+	if (c >= '0' && c <= '9')
+		return (1);
+	printf("Error\n");
+	return (0);
+}
+/**
+ * multiply - multiplies 2 #'s, prints result, must be 2 #'s
+ * @num1: factor # 1 (is the smaller of 2 numbers)
+ * @len_1: length of factor 1
+ * @num2: factor # 2 (is the larger of 2 numbers)
+ * @len_2: length of factor 2
+ * @len_r: length of result arrays
+ * Return: 0 fail, 1 success
+ */
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
+{
+	int i = 0, i1 = len_1 - 1;
+	int i2, product, carry, digit, *mul_result, *sum_result;
+
+	sum_result = _calloc(sizeof(int), (len_r));
+	while (i < len_1)
+	{
+		mul_result = _calloc(sizeof(int), len_r);
+		i2 = len_2 - 1, digit = (len_r - 1 - i);
+		if (!is_digit(num1[i1]))
+			return (NULL);
+		carry = 0;
+		while (i2 >= 0)
+		{
+			if (!is_digit(num2[i2]))
+				return (NULL);
+			product = (num1[i1] - '0') * (num2[i2] - '0');
+			product += carry;
+			mul_result[digit] += product % 10;
+			carry = product / 10;
+			digit--, i2--;
+		}
+		add_arrays(mul_result, sum_result, len_r);
+		free(mul_result);
+	    i++, i1--;
+	}
+	return (sum_result);
+}
+/**
+ * print_me - prints my array of the hopeful product here
+ * @sum_result: pointer to int array with numbers to add
+ * @len_r: length of result array
+ * Return: void
+ */
+void print_me(int *sum_result, int len_r)
+{
+	int i = 0;
+
+	while (sum_result[i] == 0 && i < len_r)
+		i++;
+	if (i == len_r)
+		_putchar('0');
+	while (i < len_r)
+		_putchar(sum_result[i++] + '0');
+	_putchar('\n');
+}
+/**
+ * main - multiply 2 input #'s of large lengths and print result or print Error
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, Success
+ */
+int main(int argc, char **argv)
+{
+	int len_1, len_2, len_r, temp, *sum_result;
+	char *num1, *num2;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
-	return (digit);
+	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+	len_r = len_1 + len_2;
+	if (len_1 < len_2)
+		num1 = argv[1], num2 = argv[2];
+	else
+	{
+		num1 = argv[2], num2 = argv[1];
+		temp = len_2, len_2 = len_1, len_1 = temp;
+	}
+	sum_result = multiply(num1, len_1, num2, len_2, len_r);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result, len_r);
+	return (0);
 }
-
-/**
-* get_prod - Multiplies a string of numbers by a single digit.
-* @prod: The buffer to store the result.
-* @mult: The string of numbers.
-* @digit: The single digit.
-* @zeroes: The neccessary number of leading zeroes.
-*
-* Description: If mult contains a non-digit, the function exits with a status value of 98.
-*/
-void get_prod(char *prod, char *mult, int digit, int zeroes)
-{
-	int mult_len, num, tens = 0;
-	
-	mult_len = find_len(mult) - 1;
-	mult + = mult_len;
-	
-	while(*prod)
-	{
-		*prod = 'x';
-		prod++;
-	}
-	
-	prod--;
-	
-	while(zeroes--)
-	{
-		*prod = '0';
-		prod--;
-	}
-	
-for(;mult_len > = 0; mult_len--, mult--, prod--)
-	
